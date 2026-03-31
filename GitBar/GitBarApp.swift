ac2d30c8f58d120eb7deececf3c4a93e7e58eee8
Pedro Ramos <pedrotr@hotmail.com>
2026-03-31 11:42:23 -0300

    Initial Commit

---
+++ GitBar/GitBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitBarApp.swift
+//  GitBar
+//
+//  Created by Pedro Ramos on 31/03/26.
+//
+
+import SwiftUI
+
+@main
+struct GitBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
